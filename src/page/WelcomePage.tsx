6b3d1cc21ae971fd2de5b5eff9eca476b2e16e7d
AndreiArtsiomenko <andartsiomenko@gmail.com>
2023-12-09 16:48:41 +0300

    feat: add styles for welcome page

--- src/page/WelcomePage.tsx
+++ src/page/WelcomePage.tsx
@@ -2,9 +2,12 @@ import { Link } from 'react-router-dom';
 
 function WelcomePage() {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-8xl font-bold text-gray-600">GraphiQL</h1>
-      <Link to={'login'} className="text-2xl mt-5 text-gray-600 underline">
+    <div className="flex flex-col w-full grow justify-center items-center absolute h-full bg-gradient-to-t from-gray-800 from-7% via-gray-600 via-10%">
+      <h1 className="text-8xl font-bold text-gray-800">GraphiQL</h1>
+      <Link
+        to={'login'}
+        className="text-2xl mt-5 text-gray-600 underline hover:text-blue-800 transition duration-200"
+      >
         Click here to start
       </Link>
     </div>
