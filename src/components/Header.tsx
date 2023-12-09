d096974a46181aff577b91efa9f43faa88bbc794
Egor Ushakov <witluv@icloud.com>
2023-12-09 11:21:55 +0300

    feat: add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+const Header = () => {
+  return (
+    <>
+      <header className="px-2 flex items-center justify-between bg-gray-800">
+        <a href="#" className="uppercase font-bold text-gray-400">
+          GraphiQL
+        </a>
+        <nav>
+          <ul className="font-semibold text-gray-400">
+            <li className="inline-block py-3 px-2 hover:text-gray-600">
+              <a href="#">Welcome</a>
+            </li>
+            <li className="inline-block py-3 px-2 hover:text-gray-600">
+              <a href="#">Language Toggle</a>
+            </li>
+            <li className="inline-block py-3 px-2 hover:text-gray-600">
+              <a href="#">Sign Out</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export { Header };
