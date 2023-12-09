54c0255f7af4b7e4c82d2dc6f9d848413a6ab93a
AndreiArtsiomenko <andartsiomenko@gmail.com>
2023-12-09 16:36:08 +0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,31 @@
+import LinkGitHub from './UI/LinkGitHub';
+
+const Footer = () => {
+  return (
+    <footer className="flex justify-between items-center shrink-0 bg-gray-800 px-8 py-1 text-white">
+      <ul>
+        <li>
+          <LinkGitHub name={'Romicusblr'} link={'https://github.com/romicusblr'} />
+        </li>
+        <li>
+          <LinkGitHub name={'W1t1uv'} link={'https://github.com/w1t1uv'} />
+        </li>
+        <li>
+          <LinkGitHub name={'AndreiArtsiomenko'} link={'https://github.com/AndreiArtsiomenko'} />
+        </li>
+      </ul>
+      <div className="font-bold">© 2023</div>
+      <div>
+        <a href="https://rs.school/react/" rel="noreferrer" target="_blank">
+          <img
+            src="./public/images/logo-rss.svg"
+            alt="logo RS School"
+            className="w-24 hover: text-blue-700"
+          />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
