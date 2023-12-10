37e5fdfd6268242fb8d644c652eb6ea5c46919ac
Raman Mastyka <raman.mastyka@gmail.com>
2023-12-10 11:15:16 +0100

    feat: add redux store

---
+++ src/store/slices/userSlice.tsx
@@ -0,0 +1,31 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from '@/store';
+import { UserAuth } from '@/types/types';
+
+const initialState: UserAuth = {
+  id: '123',
+  email: '',
+  token: '',
+};
+
+export const userSlice = createSlice({
+  name: 'userSlice',
+  initialState,
+  reducers: {
+    setUser(state, action) {
+      state.id = action.payload.id;
+      state.email = action.payload.email;
+      state.token = action.payload.token;
+    },
+    removeUser(state) {
+      state.id = '';
+      state.email = '';
+      state.token = '';
+    },
+  },
+});
+
+export const { setUser, removeUser } = userSlice.actions;
+export const selectUser = (state: RootState) => state.user;
+
+export default userSlice.reducer;
