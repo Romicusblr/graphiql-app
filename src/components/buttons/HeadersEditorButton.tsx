201226c4df605b463032a22f71e8336c4744c892
Egor Ushakov <witluv@icloud.com>
2023-12-23 23:33:06 +0300

    feat: add drop down menus with variable and header editor buttons

---
+++ src/components/buttons/HeadersEditorButton.tsx
@@ -0,0 +1,9 @@
+const HeadersEditorButton = () => {
+  return (
+    <div className="flex justify-center items-center w-2/4 h-10 cursor-pointer rounded-lg text-gray-400 hover:bg-gray-700">
+      Headers
+    </div>
+  );
+};
+
+export { HeadersEditorButton };
---
+++ src/components/buttons/VariableEditorButton.tsx
@@ -0,0 +1,9 @@
+const VariableEditorButton = () => {
+  return (
+    <div className="flex justify-center items-center w-2/4 h-10 cursor-pointer rounded-lg text-gray-400 hover:bg-gray-700">
+      Variable
+    </div>
+  );
+};
+
+export { VariableEditorButton };
