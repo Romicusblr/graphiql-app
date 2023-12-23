2bc8287f215302ea0b4cfd4b2e0d515a5c8119ae
Egor Ushakov <witluv@icloud.com>
2023-12-23 18:40:54 +0300

    feat: add a docs button in the code editor and styles for it

--- src/pages/CodeEditor.tsx
+++ src/pages/CodeEditor.tsx
@@ -1,11 +1,17 @@
 import { CodeEntry } from '@/components/CodeEditor/CodeEntry';
 import { CodeOutput } from '@/components/CodeEditor/CodeOutput';
+import { DocsButton } from '@/components/buttons/DocsButton';
 
 const CodeEditor = () => {
   return (
     <div className="flex w-full grow absolute h-full p-2 bg-gray-600 rounded-lg">
-      <CodeEntry />
-      <CodeOutput />
+      <div className="pt-1">
+        <DocsButton />
+      </div>
+      <div className="flex w-full h-full">
+        <CodeEntry />
+        <CodeOutput />
+      </div>
     </div>
   );
 };
