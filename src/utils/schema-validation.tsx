23425cac8825af7a4d1cea39440972e068e67feb
Andrei Artsiomenko <andartsiomenko@gmail.com>
2023-12-24 02:12:10 +0300

    fix: change schema validation

--- src/utils/schema-validation.tsx
+++ src/utils/schema-validation.tsx
@@ -7,9 +7,56 @@ interface Person {
 }
 
 const schema: ObjectSchema<Person> = object().shape({
-  name: string().required(),
-  email: string().email().required(),
-  password: string().min(8).max(32).required(),
+  name: string().required('Required field'),
+  email: string()
+    .email('Email must be a valid email')
+    .required('Required field')
+    .min(8, 'Email must be at least 8 characters')
+    .max(32, 'Email must be at most 32 characters')
+    .test({
+      test(value, testContext) {
+        if (!/[A-Z, a-z]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain at least one letter',
+          });
+        }
+        if (!/[0-9]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one digit',
+          });
+        }
+        if (!/[!@#$%^&*()_+\-=[\]{};':",.<>/?|\\]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one special character',
+          });
+        }
+        return true;
+      },
+    }),
+  password: string()
+    .required('Required field')
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .test({
+      test(value, testContext) {
+        if (!/[A-Z, a-z]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain at least one letter',
+          });
+        }
+        if (!/[0-9]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one digit',
+          });
+        }
+        if (!/[!@#$%^&*()_+\-=[\]{};':",.<>/?|\\]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one special character',
+          });
+        }
+        return true;
+      },
+    }),
 });
 
 export default schema;
