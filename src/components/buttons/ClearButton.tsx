4c1a7e7777e2151198564f224bad26451ed843b6
Egor Ushakov <witluv@icloud.com>
2023-12-23 18:18:18 +0300

    feat: add some styles

--- src/components/buttons/ClearButton.tsx
+++ src/components/buttons/ClearButton.tsx
@@ -2,7 +2,7 @@ import clearSvg from '../../icons/clear.svg';
 
 const ClearButton = () => {
   return (
-    <div className="w-10 h-10 flex justify-center items-center">
+    <div className="w-10 h-10 flex justify-center items-center cursor-pointer rounded-lg hover:bg-gray-700">
       <img src={clearSvg} alt="clear" className="w-10 h-10" />
     </div>
   );
--- src/components/buttons/RunButton.tsx
+++ src/components/buttons/RunButton.tsx
@@ -2,7 +2,7 @@ import runSvg from '../../icons/run.svg';
 
 const RunButton = () => {
   return (
-    <div className="bg-pink-500 rounded-lg w-10 h-10 flex justify-center items-center">
+    <div className="bg-pink-500 rounded-lg w-10 h-10 flex justify-center items-center cursor-pointer hover:bg-pink-600">
       <img src={runSvg} alt="run" className="w-4 h-4" />
     </div>
   );
