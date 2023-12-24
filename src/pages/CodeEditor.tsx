2dfe950ed72e8f38431bfcce96c2e7777f33eef7
Egor Ushakov <witluv@icloud.com>
2023-12-24 22:44:21 +0300

    feat: add a history button in the code editor and styles for it

--- src/pages/CodeEditor.tsx
+++ src/pages/CodeEditor.tsx
@@ -2,17 +2,29 @@ import { CodeEntry } from '@/components/CodeEditor/CodeEntry';
 import { CodeOutput } from '@/components/CodeEditor/CodeOutput';
 import { DocsButton } from '@/components/buttons/DocsButton';
 import { DropDownMenus } from '@/components/DropDownMenus';
+import { VariableEditor } from '@/components/VariableEditor';
+import { HistoryButton } from '@/components/buttons/HistoryButton';
 
 const CodeEditor = () => {
   return (
     <div className="flex w-full flex-col grow absolute h-full p-2 bg-gray-600">
       <div className="flex w-full h-full">
-        <div className="pt-1">
-          <DocsButton />
+        <div className="flex flex-col items-center">
+          <div className="pt-1 mb-1">
+            <DocsButton />
+          </div>
+          <div className="p-1">
+            <HistoryButton />
+          </div>
         </div>
-        <div className="flex w-full h-full">
-          <CodeEntry />
-          <CodeOutput />
+        <div className="flex flex-col w-full h-full">
+          <div className="flex w-full h-full">
+            <CodeEntry />
+            <CodeOutput />
+          </div>
+          <div className="h-2/4">
+            <VariableEditor />
+          </div>
         </div>
       </div>
       <div className="w-1/5">
