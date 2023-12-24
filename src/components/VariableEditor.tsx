3c6e0974f2321dd5b8acda70ba2046ae60b34f6a
Egor Ushakov <witluv@icloud.com>
2023-12-25 00:04:35 +0300

    feat: add a field to display when the button is clicked

--- src/components/VariableEditor.tsx
+++ src/components/VariableEditor.tsx
@@ -1,5 +1,18 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { CustomCodeMirrorProps } from '@/types';
+
 const VariableEditor = () => {
-  return <div>Hello</div>;
+  const CodeMirrorProps: CustomCodeMirrorProps = {
+    value: '',
+    options: { lineNumbers: true },
+    theme: 'none',
+  };
+
+  return (
+    <div className="relative resize-none rounded-xl outline-none border-4 border-gray-600 bg-gray-800 text-gray-400 p-2 overflow-auto w-2/6 h-full">
+      <CodeMirror {...CodeMirrorProps} />
+    </div>
+  );
 };
 
 export { VariableEditor };
--- src/components/buttons/VariableEditorButton.tsx
+++ src/components/buttons/VariableEditorButton.tsx
@@ -1,10 +1,20 @@
 import { useLocalization } from '@/context/LocalizationContext';
+import { useAppDispatch } from '@/hooks/redux';
+import { setIsOpen } from '@/store/slices/dropDownMenusSlice';
 
 const VariableEditorButton = () => {
   const { strings } = useLocalization();
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(setIsOpen(true));
+  };
 
   return (
-    <div className="flex justify-center items-center w-2/4 h-10 cursor-pointer rounded-lg text-gray-400 hover:bg-gray-700">
+    <div
+      className="flex justify-center items-center w-2/4 h-10 cursor-pointer rounded-lg text-gray-400 hover:bg-gray-700"
+      onClick={handleClick}
+    >
       {strings.variableTitle}
     </div>
   );
