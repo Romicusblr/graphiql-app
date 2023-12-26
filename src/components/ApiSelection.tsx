8e281fb5179af441a7634e0eab1f04b5c85aed2f
Raman Mastyka <raman.mastyka@gmail.com>
2023-12-26 22:19:36 +0100

    feat: implement api url change

--- src/components/ApiSelection.tsx
+++ src/components/ApiSelection.tsx
@@ -1,11 +1,23 @@
 import { useLocalization } from '@/context/LocalizationContext';
+import { selectApiUrl, setApiUrl } from '@/store/slices/appSlice';
+import { useAppSelector } from '@/hooks/redux';
+import { useDispatch } from 'react-redux';
+import { ChangeEvent } from 'react';
 
 const ApiSelection = () => {
   const { strings } = useLocalization();
+  const apiUrl = useAppSelector(selectApiUrl);
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setApiUrl(e.target.value));
+  };
 
   return (
     <input
       type="text"
+      defaultValue={apiUrl}
+      onChange={handleInputChange}
       placeholder={strings.apiSelectionPlaceholder}
       className="w-full rounded-xl outline-none p-3 mt-1 ml-2 caret-gray-400 bg-gray-800 text-gray-400 placeholder:text-gray-600"
     />
