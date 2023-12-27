ebce2efb8e85e9720884170d2ee3193f6f1025f0
Raman Mastyka <raman.mastyka@gmail.com>
2023-12-27 22:17:53 +0100

    style: run prettify

--- src/components/HeadersEditor.tsx
+++ src/components/HeadersEditor.tsx
@@ -1,21 +1,21 @@
 import CodeMirror, { ReactCodeMirrorProps } from '@uiw/react-codemirror';
 import { useLocalization } from '@/context/LocalizationContext';
-import {useAppDispatch} from '@/hooks/redux';
-import {setHeaders} from '@/store/slices/appSlice';
+import { useAppDispatch } from '@/hooks/redux';
+import { setHeaders } from '@/store/slices/appSlice';
 
 const HeadersEditor = () => {
   const { strings } = useLocalization();
-    const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-    const handleChange = (newValue: string) => {
-        dispatch(setHeaders(newValue));
-    };
+  const handleChange = (newValue: string) => {
+    dispatch(setHeaders(newValue));
+  };
 
   const CodeMirrorProps: ReactCodeMirrorProps = {
     value: '',
     basicSetup: { lineNumbers: true },
     theme: 'none',
-      onChange: handleChange,
+    onChange: handleChange,
   };
 
   return (
