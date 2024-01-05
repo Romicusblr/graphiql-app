66eda7dc49ca27fa2e205c6ec789e7438b9daed6
Egor Ushakov <witluv@icloud.com>
2024-01-05 14:40:37 +0300

    feat: change the colors for the account login form

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout: FC = () => {
   return (
     <div className="flex flex-col container mx-auto px-3 min-h-screen">
       <Header />
-      <main className="flex justify-center items-center grow relative">
+      <main className="flex bg-gray-600 justify-center items-center grow relative">
         <Outlet />
       </main>
       <Footer />
--- src/components/UI/ButtonSubmit.tsx
+++ src/components/UI/ButtonSubmit.tsx
@@ -5,7 +5,7 @@ const ButtonSubmit = ({ name }: { name: string }) => {
   return (
     <button
       type="submit"
-      className="flex w-full mt-3 justify-center rounded-md bg-gray-900 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-700"
+      className="flex w-full mt-3 justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-bold leading-6 text-gray-400 shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-700"
     >
       {name === 'login' ? strings.loginTitle : strings.registerTitle}
     </button>
