fc7152f9bee8b8c1c57ee9e058f340d3b2bbd35f
Egor Ushakov <witluv@icloud.com>
2024-01-05 20:19:56 +0300

    feat(loading): change loading title

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -3,7 +3,7 @@ import {useLocalization} from '@/hooks/localization';
 const Loading = () => {
   const { strings } = useLocalization();
 
-  return <div className="text-xl text-gray-400">{`${strings.loadingTitle}.....`}</div>;
+  return <div className="text-xl text-gray-400">{`${strings.loadingTitle}...`}</div>;
 };
 
 export default Loading;
