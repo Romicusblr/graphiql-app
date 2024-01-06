3b948c9ba1e6e7b5358515b70271addb9ad8afd0
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-06 16:51:41 +0100

    test: update buttons test

--- src/components/buttons/HeadersEditorButton.tsx
+++ src/components/buttons/HeadersEditorButton.tsx
@@ -5,12 +5,12 @@ const HeadersEditorButton: React.FC<IButtonProps> = ({ handleClick }) => {
   const { strings } = useLocalization();
 
   return (
-    <div
+    <button
       className="flex justify-center items-center cursor-pointer rounded-lg text-gray-400 hover:bg-gray-700 bg-gray-800 w-40 p-2"
       onClick={handleClick}
     >
       {strings.headersTitle}
-    </div>
+    </button>
   );
 };
 
--- src/components/buttons/buttons.test.tsx
+++ src/components/buttons/buttons.test.tsx
@@ -3,9 +3,12 @@ import { cleanup, fireEvent, render, screen } from '@testing-library/react';
 import { RunButton } from './RunButton';
 import { ClearButton } from './ClearButton';
 import { PrettifyButton } from './PrettifyButton';
+import { DocsButton } from './DocsButton';
+import { HeadersEditorButton } from './HeadersEditorButton';
+import { VariableEditorButton } from './VariableEditorButton';
 
 const handleClickMock = jest.fn();
-const buttons = [RunButton, ClearButton, PrettifyButton];
+const buttons = [RunButton, ClearButton, PrettifyButton, DocsButton, HeadersEditorButton, VariableEditorButton];
 
 describe.each(buttons)('button Component', (Component) => {
   afterEach(() => {
