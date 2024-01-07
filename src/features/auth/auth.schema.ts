8c664f0d27f4f6ce559aed2cbc398fe97eb065f5
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-07 21:46:46 +0100

    fix: fix auth schema

--- src/features/auth/auth.schema.ts
+++ src/features/auth/auth.schema.ts
@@ -1,55 +1,33 @@
 import { InferType, object, string } from 'yup';
 
 export const userLoginSchema = object({
-  email: string(),
-  // .email('Email must be a valid email')
-  // .required('Required field')
-  // .min(8, 'Email must be at least 8 characters')
-  // .max(32, 'Email must be at most 32 characters')
-  // .test({
-  //   test(value, testContext) {
-  //     if (!/[A-Z, a-z]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain at least one letter',
-  //       });
-  //     }
-  //     if (!/[0-9]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain one digit',
-  //       });
-  //     }
-  //     if (!/[!@#$%^&*()_+\-=[\]{};':",.<>/?|\\]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain one special character',
-  //       });
-  //     }
-  //     return true;
-  //   },
-  // }),
-  password: string(),
-  // .required('Required field')
-  // .min(8, 'Password must be at least 8 characters')
-  // .max(32, 'Password must be at most 32 characters')
-  // .test({
-  //   test(value, testContext) {
-  //     if (!/[A-Z, a-z]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain at least one letter',
-  //       });
-  //     }
-  //     if (!/[0-9]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain one digit',
-  //       });
-  //     }
-  //     if (!/[!@#$%^&*()_+\-=[\]{};':",.<>/?|\\]/.test(value)) {
-  //       return testContext.createError({
-  //         message: 'Should contain one special character',
-  //       });
-  //     }
-  //     return true;
-  //   },
-  // }),
+  email: string()
+    .email('Email must be a valid email')
+    .required('Required field'),
+  password: string()
+    .required('Required field')
+    .min(8, 'Password must be at least 8 characters')
+    .max(32, 'Password must be at most 32 characters')
+    .test({
+      test(value, testContext) {
+        if (!/[A-Z, a-z]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain at least one letter',
+          });
+        }
+        if (!/[0-9]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one digit',
+          });
+        }
+        if (!/[!@#$%^&*()_+\-=[\]{};':",.<>/?|\\]/.test(value)) {
+          return testContext.createError({
+            message: 'Should contain one special character',
+          });
+        }
+        return true;
+      },
+    }),
 });
 
 export const userRegisterSchema = userLoginSchema.shape({
