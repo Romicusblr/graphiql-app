544017dd6054254354ffa2e70e01b988c9513935
Raman Mastyka <raman.mastyka@gmail.com>
2024-01-07 12:19:43 +0100

    test: add schema test

--- src/features/editor/Schema.tsx
+++ src/features/editor/Schema.tsx
@@ -1,23 +1,17 @@
 import { useGetSchemaQuery } from '@/app/services/graphql';
-import { buildClientSchema, printSchema } from 'graphql/index';
 
 const Schema = () => {
   const { data } = useGetSchemaQuery(undefined);
-  let printedSchema;
-
-  if (data) {
-    const result = data.data;
-    const schema = buildClientSchema(result);
-    printedSchema = printSchema(schema);
-  }
 
   return (
-    <textarea
-      name="docs"
-      value={printedSchema}
-      className="text-base h-5/6 w-full resize-none outline-none bg-gray-800 text-gray-400 p-4"
-      readOnly
-    />
+    data && (
+      <textarea
+        name="docs"
+        value={data}
+        className="text-base h-5/6 w-full resize-none outline-none bg-gray-800 text-gray-400 p-4"
+        readOnly
+      />
+    )
   );
 };
 
